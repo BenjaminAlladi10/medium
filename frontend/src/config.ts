b4d1bcd476c792d73e0d4d1c3aa3a95f92a381bb
nikhil <21bd1a1283itb@gmail.com>
2024-12-09 05:03:59 +0530

    ish

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = "https://backend.21bd1a1283itb.workers.dev"
 // export const BACKEND_URL = "http://localhost:8787"
-https://backend.21bd1a1283itb.workers.dev
+// https://backend.21bd1a1283itb.workers.dev
