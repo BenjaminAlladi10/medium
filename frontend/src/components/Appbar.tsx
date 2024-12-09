12ec33ab597829fe43ba11ac8701f55e211a52a4
Benjamin <benjaminalladi10@gmail.com>
2024-12-09 18:16:03 +0530

    added blogContext and BlogSummary frontend

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -38,7 +38,7 @@ export const Appbar= ()=>{
                     viewBox="0 0 24 24"
                     aria-label="Profile"
                     className="mr-2">
-                    <circle cx="12" cy="7" r="4.5" stroke="currentColor"></circle><path stroke="currentColor" stroke-linecap="round"
+                    <circle cx="12" cy="7" r="4.5" stroke="currentColor"></circle><path stroke="currentColor" strokeLinecap="round"
                     d="M3.5 21.5v-4.342C3.5 15.414 7.306 14 12 14s8.5 1.414 8.5 3.158V21.5"></path></svg>
                 Signout
                 </button> </Link>
